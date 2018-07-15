2686b99ce5e2f5f5bc5e4a5298b8a530d6f50d34
Juan José Granados <juanjogramo@gmail.com>
2018-07-15 14:08:26 -0500

    Solicitar servicio en progreso

---
+++ app/src/main/java/pe/tp1/hdpeta/jalame/Activity/Dialog/ConfirmRequestDialogActivity.java
@@ -0,0 +1,15 @@
+package pe.tp1.hdpeta.jalame.Activity.Dialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.tp1.hdpeta.jalame.R;
+
+public class ConfirmRequestDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_request_dialog);
+    }
+}
